46ac9ea37897caf2d2e61a70a5e09fd0952a6d5b
Lukas-Fohl <Lukas@Fohl.net>
2025-07-26 19:57:51 +0200

    add nvim save + new tmux colors

--- .config/nvim/lua/lukas/remap.lua
+++ .config/nvim/lua/lukas/remap.lua
@@ -5,9 +5,9 @@ vim.keymap.set("n", "<C-j>", "}")
 vim.keymap.set("v", "<C-k>", "{")
 vim.keymap.set("v", "<C-j>", "}")
 
-vim.keymap.set("i", "<C-s>", "<esc><return>")
-vim.keymap.set("n", "<C-s>", "<esc><return>")
-vim.keymap.set("v", "<C-s>", "<esc><return>")
+vim.keymap.set("i", "<C-s>", "<esc>:w<return>")
+vim.keymap.set("n", "<C-s>", "<esc>:w<return>")
+vim.keymap.set("v", "<C-s>", "<esc>:w<return>")
 
 vim.keymap.set("t", "<esc>", "<C-\\><C-n>")
 
